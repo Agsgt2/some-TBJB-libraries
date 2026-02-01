6a3abcea481351748408d176779e9ed4293527d9
Agsgt2 <angelthefourth20242026@gmail.com>
2026-02-01 17:27:43 -0600

    Rename messageSender to messageReceiver and update logic

--- anti-67.js
+++ anti-67.js
@@ -1,3 +1,4 @@
-addons.register('messageSender', function(data) {
+addons.register('messageReciever' /* Makes it global (client side)*/, function(data) {
   data.msg = data.msg.replaceAll('67', 'The Number')
+  data.msg = data.msg.replaceAll("SIX SEVEN", "THE NUMBER")
 });
