8b2c2571b2f7307bd7cfdc81a3beedf27f6c01fe
Agsgt2 <angelthefourth20242026@gmail.com>
2026-03-13 17:50:54 -0600

    Refactor hate management to use 'home' instead of 'nick'

--- remove-hated.js
+++ remove-hated.js
@@ -1,7 +1,7 @@
 let H = []
 
 addons.register('messageReciever', function(data) {
-    if (H.includes(data.nick)){
+    if (H.includes(data.home)){
         data.msg = ""
     }
 });
@@ -9,23 +9,26 @@ addons.register('messageReciever', function(data) {
 addons.register('messageSender', function(data) {
     if (data.msg === "/hate"){
         data.msg = ""
-        let hated = prompt("Enter who to be hated:")
+        let hated = prompt("Enter home of who to be hated:")
         if (H.includes(hated) != true){
             H.push(hated)
             alert(hated+" hated!")
         } else {
-            alert("User "+hated+" already exists!")
+            alert("Home "+hated+" already exists!")
         }
     }
     if (data.msg === "/unhate"){
       data.msg = ""
-      let unhated = prompt("Enter who to be unhated:")
+      let unhated = prompt("Enter the home of who to be unhated:")
       const index = H.indexOf(unhated);
       if (index !== -1) {
           H.splice(index, 1);
           alert(unhated+" unhated!")
       } else {
-          alert("User "+unhated+" is not hated!")
+          alert("Home "+unhated+" is not hated!")
       }
-  }
+    }
+    if (data.msg === "/hated"){
+        alert("The hated homes are:\n"+H.join("\n"))
+    }
 });
