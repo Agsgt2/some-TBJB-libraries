8e13209320b72d9c5c440ea4d2ae85953f070ee1
Agsgt2 <angelthefourth20242026@gmail.com>
2026-03-13 17:52:53 -0600

    Refactor eval usage for messageSender addon

--- javascript-on-trollbox.js
+++ javascript-on-trollbox.js
@@ -1,5 +1,5 @@
 addons.register('messageSender', function(data) {
   if (data.msg === "/run"){
-    data.msg=eval(prompt("Enter a javascript function:", "alert('Hello, World!')"))
+    data.msg=`${eval(prompt("Enter a javascript function:", "alert('Hello, World!')"))}`
   }
 });
